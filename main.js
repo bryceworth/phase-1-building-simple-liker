bee502b2b735178fb0ea05a5d3df921033bbb1a9
bryceworth <bryceaworth@gmail.com>
2022-05-04 19:30:27 -0700

    commit tree 6d5fc3a609a228fc05c6a95610ee66e3025b500a

--- main.js
+++ main.js
@@ -1,9 +1,35 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+let heartsNodeArray = [...document.getElementsByClassName(`like-glyph`)];
+let modal = document.getElementById(`modal`);
+let modalParagraph = document.getElementById(`modal-message`)
 
-// Your JavaScript code goes here!
+ let callServerAndCatch = () => {
+    try {
+    throw mimicServerCall()
+        }  
+    catch (error) {
+    handleError(error)
+                  } 
+    finally   {
+    console.log(`no errors`)
+              }
+          }
 
+let handleError = (errorMessage) => {
+ modal.classList.remove(`hidden`)
+ modalParagraph.innerText = errorMessage
+ setTimeout(() => {
+   modal.classList.add(`hidden`)
+  modal.modalParagraph.innerText = ``
+}, 3000);
+}
+
+
+heartsNodeArray.map(heartNode => {
+heartNode.addEventListener(`click`, callServerAndCatch)  
+})
 
 
 
@@ -12,6 +38,7 @@ const FULL_HEART = '♥'
 //------------------------------------------------------------------------------
 
 function mimicServerCall(url="http://mimicServer.example.com", config={}) {
+ console.log(`clicked!`)
   return new Promise(function(resolve, reject) {
     setTimeout(function() {
       let isRandomFailure = Math.random() < .2
